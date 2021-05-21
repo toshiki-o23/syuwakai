d2eff59caa4a77c592db754099437c24ae2e330a
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-21 15:11:46 +0900

    [fix] environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
-)
