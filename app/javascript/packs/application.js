2fe4eb1a65c9b308895ac1686f375948654c1102
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-21 15:35:01 +0900

    [fix] gemfile bootstrapバージョン指定なし

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import '../stylesheets/application';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -22,5 +25,4 @@ require("channels")
 //= require bootstrap
 //= require_tree .
 
-import 'bootstrap';
-import '../stylesheets/application';
+
