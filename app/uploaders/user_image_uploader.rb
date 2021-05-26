0ea2b17768ae839a2822c82ee480633c74e201cd
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-26 11:44:47 +0900

    [fix] event index 画像表示

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -14,13 +14,13 @@ class UserImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  # def default_url(*args)
     #   # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    #
-    "default.jpeg"
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    # #
+    # "default.jpeg"
     #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  # end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
