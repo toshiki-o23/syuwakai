22777b064a133c832edc7e77e05cc3c443b98a78
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-30 15:02:36 +0900

    [fix] test mysql connect

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Test, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Diff, type: :model do
+  it "is valid with diff string" do
+    diff = Diff.new(diff: "some diff")
+    expect(diff).to be_valid
+  end
 end
