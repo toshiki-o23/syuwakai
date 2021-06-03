179d622ec518df0aff65c4b72eb7e126039142dc
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-03 18:08:24 +0900

    [fix] user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- it "is valid with a name and email"
+ it "is valid with a name and email, password"
  it "is invalid without a name"
 end
