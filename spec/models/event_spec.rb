4adab207bf112efa3ed735075ec0e88e2737c798
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-05 19:26:12 +0900

    [add] event model test

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  before do
+    @event = FactoryBot.build(:event)
+  end
+
+  describe '新規投稿' do
+    context '新規投稿ができたとき' do
+      it "内容に問題ない場合" do
+        expect(@event).to be_valid  
+      end
+      it "titleが30文字以下の投稿が可能" do
+        @event.title = 'a' * 30
+        expect(@event).to be_valid  
+      end
+    end
+
+    context "新規投稿がうまくいかないとき" do
+      it "titleが空の場合、投稿できない" do
+        @event.title = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("タイトルを入力してください")
+      end
+      
+    end
+    
+  end
+end
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Events", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
 end
