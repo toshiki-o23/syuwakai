7ca7254f9f384da4b2ff5fccb7c91e74fdff1984
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-16 03:36:28 +0900

    docker whitelist ip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.web_console.whitelisted_ips = '172.27.0.1'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
