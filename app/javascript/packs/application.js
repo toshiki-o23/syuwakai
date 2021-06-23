1d357fd18c3a393309ee1cd270212bdd575ebe3b
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-23 17:05:41 +0900

    [fix] packs application.js resolve path app/assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ require("@fortawesome/fontawesome-free");
 
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application.scss";
+import '../stylesheets/application.scss';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
