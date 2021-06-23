d38dec8b4760dfd83cbc09e249326a6ac940d82b
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-23 23:18:06 +0900

    [fix] ユーザーフォロー・フォロワー一覧　見た目修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  process resize_to_fill: [200, 200, 'Center']
+  process resize_to_fill: [1280, 720]
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
