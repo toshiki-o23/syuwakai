4a8a8c29c39ee95bc3b8c94c09dbeb17ca9a6ca7
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-26 19:31:30 +0900

    aaa

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@
 require 'capybara/rspec'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.include Capybara::DSL
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
