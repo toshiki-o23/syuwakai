139db46e0112c98849222171a68e2df75621ba7a
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-26 19:35:14 +0900

    [delete] feature test

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,20 +1,20 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.feature "Events", type: :feature do
-  scenario '投稿可能' do
-    user = FactoryBot.build(:user)
-    sign_up_as user
+# RSpec.feature "Events", type: :feature do
+#   scenario '投稿可能' do
+#     user = FactoryBot.build(:user)
+#     sign_up_as user
 
-    expect(page).to have_content('新規投稿')
+#     expect(page).to have_content('新規投稿')
 
-    expect {
-      click_link('新規投稿')
-      expect(current_path).to eq new_event_path
-      fill_in 'event_title', with: '手話勉強会'
-      fill_in 'event_content', with: '手話を勉強しよう！'
-      find('input[type="submit"]').click
-    }.to change(Event, :count).by(1)
+#     expect {
+#       click_link('新規投稿')
+#       expect(current_path).to eq new_event_path
+#       fill_in 'event_title', with: '手話勉強会'
+#       fill_in 'event_content', with: '手話を勉強しよう！'
+#       find('input[type="submit"]').click
+#     }.to change(Event, :count).by(1)
 
-    expect(page).to have_content('手話勉強会')
-  end
-end
+#     expect(page).to have_content('手話勉強会')
+#   end
+# end
