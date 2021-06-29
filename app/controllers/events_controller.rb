dd4154650e6cc3ffecb7dab5e5bad4668d500ed7
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-06-29 17:28:55 +0900

    [fix] material icon header

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,9 +4,6 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    # ransack gem よる検索機能
-    @q = Event.ransack(params[:q])
-    @events = @q.result(distinct: true)
   end
 
   def new
