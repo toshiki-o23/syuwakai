e5a3e6e248525cd9d356f956ab5132abf3c5b2c6
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-07-09 22:23:44 +0900

    [fix] start_time表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,7 +45,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :content, :image, :level, :venue, :start_datetime, :finish_datetime).merge(user_id: current_user.id)
+    params.require(:event).permit(:title, :content, :image, :level, :venue, :start_time, :finish_time).merge(user_id: current_user.id)
   end
 
   def set_event
