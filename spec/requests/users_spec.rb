a50574b6a407124c823db9962dd9f1505ede8176
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-09-04 17:57:42 +0900

    [add] user bookmark 統合テスト

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe "ログイン済み" do
+    it "ユーザー詳細画面 show" do
+      sign_in @user
+      get user_path(@user)
+      expect(response).to have_http_status(200)
+      expect(response.body).to include 'test_user_1'
+    end
+  end
+end
