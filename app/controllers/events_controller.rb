ff2eef1a33b50df634e4ece6cd0009f5ae4353fa
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-09-07 16:03:15 +0900

    [fix] イベント一覧の標準順番を開催日が近い順とする

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -78,18 +78,18 @@ class EventsController < ApplicationController
     # 検索結果orタグ検索結果orフォローしたユーザーの投稿一覧or投稿一覧
     if params[:q].present?
       @q = Event.ransack(params[:q])
-      @events = @q.result.includes(%i[user tags tagmaps]).where('start_time > ?', DateTime.now)
+      @events = @q.result.includes(%i[user tags tagmaps]).where('start_time > ?', DateTime.now).order(start_time: :asc)
     elsif params[:tag_id].present?
       params[:q] = { sorts: 'id desc' }
       @tag = Tag.find(params[:tag_id])
-      @events = @tag.events.includes(%i[user tags tagmaps]).where('start_time > ?', DateTime.now)
+      @events = @tag.events.includes(%i[user tags tagmaps]).where('start_time > ?', DateTime.now).order(start_time: :asc)
     elsif params[:follow_event_id].present?
       params[:q] = { sorts: 'id desc' }
       @current_user = User.find(params[:follow_event_id])
-      @events = Event.includes(%i[user tags tagmaps]).where(user_id: @current_user.following_ids)
+      @events = Event.includes(%i[user tags tagmaps]).where(user_id: @current_user.following_ids).order(start_time: :asc)
     else
       params[:q] = { sorts: 'id desc' }
-      @events = Event.all.includes(%i[user tags tagmaps]).where('start_time > ?', DateTime.now)
+      @events = Event.all.includes(%i[user tags tagmaps]).where('start_time > ?', DateTime.now).order(start_time: :asc)
     end
   end
 end
